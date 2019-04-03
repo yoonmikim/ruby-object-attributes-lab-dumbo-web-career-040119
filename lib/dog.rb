f01f5f4d74ed7eaf54b215be47492593b2721021
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-03 02:27:27 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(this_name)
+    @this_name = this_name
+  end
+  
+  def name
+    @this_name
+  end
+  
+   def breed=(this_breed)
+    @this_breed = this_breed
+  end
+  
+  def breed
+    @this_breed
+  end
+end
